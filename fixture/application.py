1292af1d566c5f862d9e83e12b29b38321cf6461
Dima <aslam10_dima@mail.ru>
2020-03-07 11:40:01 +0300

    Выделен класс для работы с контактами

--- fixture/application.py
+++ fixture/application.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 class Application:
     def __init__(self):
@@ -9,100 +9,11 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self, wd):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def open_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_new_group(self, group):
-        wd = self.wd
-        self.open_group_page()
-        # create new group
-        wd.find_element_by_name("new").click()
-        # fill group from
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # complete create
-        wd.find_element_by_name("submit").click()
-
-    def open_add_form(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def create_new_contact(self, contact):
-        wd = self.wd
-        self.open_add_form()
-        # filling fields
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email1)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.home_page)
-        # filling Birthday
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Birthday:'])[1]/following::option[18]").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_xpath("//option[@value='July']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        # conplete create
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-    
-    def back_to_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
 
     def back_to_home(self):
         wd = self.wd
--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import pytest
-import unittest
 from model.contact import Contact
 from fixture.application import Application
 
@@ -12,12 +11,9 @@ def app(request):
 
 def test_add_contact(app):
         app.session.login(username="admin", password="secret")
-        app.create_new_contact(Contact(first_name =u"Иван", middle_name =u"Иванович", last_name =u"Иванов",
-                                            nickname = "Ivan!", company = "Booking Mashin", title = "Ivyshka",
-                                            address = "Pyshkina st home Kolotyshkina 5", home = "123456789", mobile = "+987654321",
-                                            work = "14946", fax = "228", email1 = "123@mail.ru", email2 = "321@mail.ru",
-                                            email3 = "333@mail.ru", home_page = "ok.ru", bday = "16", bmonth = "July", byear = "1995"))
+        app.contact.create(Contact(first_name =u"Иван", middle_name =u"Иванович", last_name =u"Иванов",
+                                   nickname = "Ivan!", company = "Booking Mashin", title = "Ivyshka",
+                                   address = "Pyshkina st home Kolotyshkina 5", home = "123456789", mobile = "+987654321",
+                                   work = "14946", fax = "228", email1 = "123@mail.ru", email2 = "321@mail.ru",
+                                   email3 = "333@mail.ru", home_page = "ok.ru", bday = "16", bmonth = "July", byear = "1995"))
         app.session.logout()
-
-if __name__ == "__main__":
-    unittest.main()
