a81e9141629956341710f5d117beae627f5975cf
Dima <aslam10_dima@mail.ru>
2020-03-09 21:42:21 +0300

    Доработал тест изменения группы используя конструкцию if

--- tests/test_edit_first_group.py
+++ tests/test_edit_first_group.py
@@ -4,3 +4,8 @@ def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_first(Group(name="His name", header="His header", footer="His footer"))
     app.session.logout()
+
+def test_edit_first_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first(Group(name="New name"))
+    app.session.logout()
