ee42e23e6df42c92f1573ad7ce570f785ab4525d
Dima <aslam10_dima@mail.ru>
2020-03-14 14:14:04 +0300

    добавлена проверка на существование хотя бы одного контакта или группы перед изменением соответствующей записи

--- tests/test_edit_first_contact.py
+++ tests/test_edit_first_contact.py
@@ -2,6 +2,13 @@ from model.contact import Contact
 import time
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name =u"Иван", middle_name =u"Иванович", last_name =u"Иванов",
+                                   nickname = "Ivan!", company = "Booking Mashin", title = "Ivyshka",
+                                   address = "Pyshkina st home Kolotyshkina 5", home = "123456789", mobile = "+987654321",
+                                  work = "14946", fax = "228", email1 = "123@mail.ru", email2 = "321@mail.ru",
+                                  email3 = "333@mail.ru", home_page = "ok.ru", bday = "16", bmonth = "July", byear = "1995"))
+        app.contact.back_to_home()
     app.contact.edit_first(Contact(first_name=u"Петр", middle_name=u"Петрович", last_name=u"Петров",
                                    nickname="Petr!", company="Petrs Mashin", title="Pet9",
                                    address="Pyshkina st home Kolotyshkina 6", home="987654321", mobile="+12345678",
--- tests/test_edit_first_group.py
+++ tests/test_edit_first_group.py
@@ -1,7 +1,11 @@
 from model.group import Group
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="N", header="H", footer="F"))
     app.group.edit_first(Group(name="His name", header="His header", footer="His footer"))
 
 def test_edit_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="N", header="H", footer="F"))
     app.group.edit_first(Group(name="New name"))
