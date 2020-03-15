ef2ffe6496e6f4e914f3798125ab4f62af0d98b2
Dima <aslam10_dima@mail.ru>
2020-03-15 15:18:24 +0300

    Добавлена проверка сравнения списка групп после добавления новой группы

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -3,12 +3,18 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="Her name", header="Her header", footer="Her footer"))
+    test_group = Group(name="Her name", header="Her header", footer="Her footer")
+    app.group.create(test_group)
     new_groups = app.group.get_group_list()
     assert(len(old_groups) + 1 == len(new_groups))
+    old_groups.append(test_group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_add__empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    test_group = Group(name="", header="", footer="")
+    app.group.create(test_group)
     new_groups = app.group.get_group_list()
     assert(len(old_groups) + 1 == len(new_groups))
+    old_groups.append(test_group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
